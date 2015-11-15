262f20e1d41c04eded19e2194dc9e42366a6117c
Chen Ye <cye@brown.edu>
2015-11-15 01:35:09 +0000

    log password on new user creation (stopgap); better password generation

--- hms/server/startup.jsx
+++ hms/server/startup.jsx
@@ -1,6 +1,11 @@
-/* 
+/* global Random */
+/* global Roles */
+/* global _ */
+
+/*
  * On startup, initialize the users found in the adminSetup.json file in /private/config.
  */
+
 
 Meteor.startup(function () {
   //Make sure that the database is empty.
@@ -9,7 +14,10 @@ Meteor.startup(function () {
 
     _.each(users, function (user) {
       var id;
-      var tempPassword = Math.random().toString(36).substr(2, 5);
+      var tempPassword = Random.secret(10);
+
+      //TODO: Unsafe - this is just a stopgap!
+      console.log(user.email, tempPassword);
 
       id = Accounts.createUser({
         email: user.email,
@@ -18,7 +26,7 @@ Meteor.startup(function () {
       });
 
       if (user.roles.length > 0) {
-        // Need _id of existing user record so this call must come 
+        // Need _id of existing user record so this call must come
         // after `Accounts.createUser` or `Accounts.onCreate`
         for (var i=0; i < user.roles.length; i++) {
           Roles.addUsersToRoles(id, user.roles[i]);
@@ -26,4 +34,4 @@ Meteor.startup(function () {
       }
     });
   }
-}); 
+});
